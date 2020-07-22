0a6ffb2133ce871b1ef196d45dbcf874babfc968
lawrencedarcy <lawrencedwakefield@gmail.com>
2020-07-22 11:14:59 +0100

    style polling card

--- client/src/Poll/Poll.js
+++ client/src/Poll/Poll.js
@@ -4,10 +4,12 @@ import './Poll.css';
 function Poll(props) {
   return (
     <div className='poll-wrapper'>
-
+    <div className='poll-title'>Poll of the day</div>
+    <div className='poll-inner-wrapper'>
   <div className='poll-question'>Should we bring back the death penalty?</div>
-  <div className='poll-answer'>Yes</div>
-  <div className='poll-answer'>No</div>
+  <div className='poll-answer'> <span className='emoji'>✅</span> Yes</div>
+  <div className='poll-answer'> <span className='emoji'>❌</span> No</div>
+  </div>
     </div>
   );
 }
